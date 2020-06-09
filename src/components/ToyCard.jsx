6c1a19e086132ea1a9050b6205caeb2dc85685d3
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-06-08 23:02:22 -0500

    Done.

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 
 class ToyCard extends Component {
 
+  constructor(props) {
+    super()
+
+    this.state = {
+      likes: props.toy.likes
+    }
+  }
+
+  updateLikesCount = (event) => {
+    const toyId = this.props.toy.id
+    const toyURL = `http://localhost:5000/toys/${toyId}`
+
+    let newLikeCount = { likes: this.props.toy.likes += 1  }
+
+    const configObj = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(newLikeCount)
+    }
+
+    fetch(toyURL, configObj)
+      .then(resp => resp.json())
+      .then(toy => {
+        this.setState({
+          likes: this.props.toy.likes
+        })
+      })
+  }
+
+  deleteToyClick = () => {
+    const toyId = this.props.toy.id
+    this.props.onDeleteToyClick(toyId)
+  }
+
   render() {
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={/* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
-        <button className="like-btn">Like {'<3'}</button>
-        <button className="del-btn">Donate to GoodWill</button>
+        <h2>{this.props.toy.name}</h2>
+        <img src={this.props.toy.image} className="toy-avatar" />
+        <p>{this.state.likes} Likes</p>
+        <button onClick={this.updateLikesCount} className="like-btn">Like {'<3'}</button>
+        <button onClick={this.deleteToyClick} className="del-btn">Donate to GoodWill</button>
       </div>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      name: '',
+      image: '',
+      likes: 0
+    }
+  }
+
+  onNewToyInputChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  onNewToySubmit = (event) => {
+    event.preventDefault()
+    let newToy = this.state
+    this.props.addNewToy(newToy)
+
+    this.setState({
+      name: '',
+      image: ''
+    })
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.onNewToySubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input onChange={this.onNewToyInputChange} value={this.state.name} type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input onChange={this.onNewToyInputChange} value={this.state.image} type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
